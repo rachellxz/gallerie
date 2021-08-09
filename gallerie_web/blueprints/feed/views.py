fbad9c4277b50136b281ea4db56f076527808e22
rachel <rachelxzlim@gmail.com>
2021-08-09 13:43:43 +0800

    add upload image to feed

--- gallerie_web/blueprints/feed/views.py
+++ gallerie_web/blueprints/feed/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.user import User
+from models.feed import Feed
 from flask_login import login_user, login_required, current_user
 from app import app
-import datetime
 from gallerie_web.util.helpers import upload_file_to_s3, allowed_file
 from werkzeug.utils import secure_filename
 
@@ -14,11 +14,35 @@ def index():
     return redirect(url_for('home'))
 
 
-# upload image to feed
-@feed_blueprint.route('/new', methods=["GET", "POST"])
+@feed_blueprint.route("/new", methods=["GET"])
 def new():
-    if request.method == "GET":
-        return redirect(url_for("home"))
+    return redirect(url_for("home"))
 
-    if request.method == "POST":
-        return "Uploading post!"
+
+# upload image to feed
+@feed_blueprint.route("/create", methods=["POST"])
+def create():
+    user = User.get_or_none(User.username == current_user.username)
+
+    if "user_file" not in request.files:
+        flash("No file selected!")
+        return render_template("users/show.html", user=user)
+
+    file = request.files["user_file"]
+
+    if file.filename == "":
+        flash("Please select an image with valid filename to upload.")
+        return render_template("users/show.html", user=user)
+
+    if file and allowed_file(file.filename):
+        file.filename = secure_filename(file.filename)
+        #output is the URL path
+        output = upload_file_to_s3(file, app.config["S3_BUCKET"])
+
+        # save the img URL path to new_user_feed in the Feed table, current_user.id as the user_id
+        new_user_feed = Feed(user=current_user.id, image_url=str(output))
+
+        if new_user_feed.save():
+            flash("Your post has been successfully uploaded!")
+            return redirect(
+                url_for('users.show', username=current_user.username))
